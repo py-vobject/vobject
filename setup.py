05e65de527254c12d7626b64c20864b865f4868c
Lucas Hoffmann <lucc@posteo.de>
2024-04-09 21:12:00 +0200

    Move some more setuptools settings to pyproject

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(zip_safe = True,
-      include_package_data = True,
-      install_requires=["python-dateutil >= 2.5.0; python_version < '3.10'",
-                        "python-dateutil >= 2.7.0; python_version >= '3.10'",
-                        "pytz", 'six'],
-      platforms = ["any"],
       packages = find_packages(),
       test_suite="tests",
       )
