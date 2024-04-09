acc0980a3f5a7162880ac0d6051aa538ef6a0386
Lucas Hoffmann <lucc@posteo.de>
2024-04-09 21:11:25 +0200

    Replace long_description with readme in pyproject

--- setup.py
+++ setup.py
@@ -1,31 +1,4 @@
-"""
-VObject: module for reading vCard and vCalendar files
-
-Description
------------
-
-Parses iCalendar and vCard files into Python data structures, decoding the
-relevant encodings. Also serializes vobject data structures to iCalendar, vCard,
-or (experimentally) hCalendar unicode strings.
-
-Requirements
-------------
-
-Requires python 2.7 or later, dateutil 2.4.0 or later and six.
-
-Recent changes
---------------
-    - Revert too-strict serialization of timestamp values - broke too many other
-       implementations
-
-For older changes, see
-   - http://py-vobject.github.io/#release-history or
-   - http://vobject.skyhouseconsulting.com/history.html
-"""
-
 from setuptools import setup, find_packages
-
-doclines = (__doc__ or '').splitlines()
 
 setup(zip_safe = True,
       include_package_data = True,
@@ -34,6 +7,5 @@ setup(zip_safe = True,
                         "pytz", 'six'],
       platforms = ["any"],
       packages = find_packages(),
-      long_description = "\n".join(doclines[2:]),
       test_suite="tests",
       )
